65e234a30744ecbb9844c0e797e45d5c8f79fb35
SakuraSmiles <306596878@qq.com>
2018-09-21 12:37:36 +0800

    springMVC+Maven基础实例

---
+++ src/main/java/com/groupA/alpha/CoreController.java
@@ -0,0 +1,27 @@
+package com.groupA.alpha;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+
+
+@Controller
+public class CoreController {
+
+	String message = "Welcome to Spring MVC!";
+	 
+    @RequestMapping("/hello")
+    public ModelAndView showMessage(@RequestParam(value = "name", required = false, defaultValue = "Spring") String name) {
+ 
+        ModelAndView mv = new ModelAndView("hello");//÷∏∂® ”Õº
+
+        mv.addObject("message", message);
+        mv.addObject("name", name);
+        return mv;
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,8 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<html>
+<body>
+        <h2>Hello World</h2>
+        <h3>
+            <a href="hello?name=SakuraSmiles">点击跳转</a>
+        </h3>
+</html>
