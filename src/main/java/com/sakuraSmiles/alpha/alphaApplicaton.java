e8a9b3c297eec28355b48543b4e0e9f04dfec6cc
SakuraSmiles <306596878@qq.com>
2018-11-15 15:05:26 +0800

    change to start server with jetty

---
+++ src/main/java/com/sakuraSmiles/alpha/alphaApplicaton.java
@@ -0,0 +1,12 @@
+package com.sakuraSmiles.alpha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class alphaApplicaton {
+    public static void main(String[] args){
+        SpringApplication.run(alphaApplicaton.class, args);
+    }
+
+}
---
+++ src/main/java/com/sakuraSmiles/alpha/config/springMvcConfigure.java
@@ -0,0 +1,10 @@
+package com.sakuraSmiles.alpha.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@Configuration
+public class springMvcConfigure extends WebMvcConfigurationSupport  {
+
+
+}
